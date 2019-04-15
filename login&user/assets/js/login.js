1167e61dd1355caa7e3b587cc36e37aed1b60abc
forerverF91 <1006244759@qq.com>
2019-04-15 22:43:40 +0800

    login error reporting

--- login&user/assets/js/login.js
+++ login&user/assets/js/login.js
@@ -1,13 +1,26 @@
 var username = document.getElementById("form-username");
+var password = document.getElementById("form-password");
 username.addEventListener('blur',function(){
     var strRegex = "[a-z0-9A-Z]+";
     var re=new RegExp(strRegex);
     if(re.test(username.value) != true){
         t = 1;
-        username.style.border = "1px solid red";
+        username.style.border = "2px solid red";
     }
     else{
-        username.style.border = "1px solid green";
+        username.style.border = "2px solid green";
+    }
+	
+});
+
+password.addEventListener('blur',function(){
+    var strRegex = "[a-z0-9A-Z]+";
+    var re=new RegExp(strRegex);
+    if(re.test(password.value) != true){
+		password.style.border = "2px solid red";
+	}
+	else{
+		password.style.border = "2px solid green";
     }
 });
 
@@ -22,9 +35,13 @@ document.getElementById("signin_btn").addEventListener("click", function (e) {
     e.preventDefault();
     var username = document.getElementById("form-username").value;
     var password = document.getElementById("form-password").value;
+	var strRegex = "[a-z0-9A-Z]+";
+    var re=new RegExp(strRegex);
     document.cookie = "username" + username;
     
     
+	
+	
     //get the value of the radio button
     var radios = document.getElementsByName("type");
     var value;
@@ -41,11 +58,19 @@ document.getElementById("signin_btn").addEventListener("click", function (e) {
         "password": password
     };
     if(username == ""){
-        error_message.innerHTML = "The Password cam't be Wrong!"
+        error_message.innerHTML = "The Username cam't be Blank!"
     }
     else{
+		if(re.test(username) != true){
+			error_message.innerHTML = "Username format error, use 'a-z,0-9,A-Z'"
 
-        $.ajax({
+		}
+		else if(re.test(password) != true){
+			error_message.innerHTML = "Password format error"
+
+		}
+		else{
+			$.ajax({
             type: "POST",
             url: 'http://10.19.42.253:5000/account/login',
             data: JSON.stringify(text),
@@ -71,7 +96,11 @@ document.getElementById("signin_btn").addEventListener("click", function (e) {
                 }
                 
             }
-        }) 
+        })
+		}
+		
+		
+         
     }
 });
 
