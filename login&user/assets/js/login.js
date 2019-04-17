43815e3e478781589fd504b9cab1ae709bee30b1
forerverF91 <1006244759@qq.com>
2019-04-17 10:25:35 +0800

    add remember password

--- login&user/assets/js/login.js
+++ login&user/assets/js/login.js
@@ -1,3 +1,19 @@
+//when the explore is ready
+$(document).ready(function(){
+    var remember = document.cookie.split(";")[0].split("=")[1];
+    var username = document.cookie.split(";")[1].split("=")[1];
+    var password = document.cookie.split(";")[2].split("=")[1];
+    if(remember != "undefined"){
+        //顺序是乱的不知道为什么
+        $("#remember").prop("checked",true);
+        $("#form-username").val(remember);
+        $("#form-password").val(username);
+
+    }
+});
+
+
+
 var username = document.getElementById("form-username");
 var password = document.getElementById("form-password");
 username.addEventListener('blur',function(){
@@ -13,6 +29,10 @@ username.addEventListener('blur',function(){
 	
 });
 
+
+
+
+
 password.addEventListener('blur',function(){
     var strRegex = "[a-z0-9A-Z]+";
     var re=new RegExp(strRegex);
@@ -23,6 +43,22 @@ password.addEventListener('blur',function(){
 		password.style.border = "2px solid green";
     }
 });
+
+//save the cookies
+function save_cookies(){
+    if($("#remember").prop("checked")){
+        var username = $("#form-username").val();
+        var password = $("#form-password").val();
+
+        document.cookie="username="+username;
+        document.cookie="password="+password;
+        document.cookie="remember="+"true";
+    }else{
+        document.cookie="username="+username;
+        document.cookie="password="+password;
+        document.cookie="remember="+"";
+    }
+};
 
 //test the error message
 var error_message = document.getElementById("error_message");
@@ -37,8 +73,10 @@ document.getElementById("signin_btn").addEventListener("click", function (e) {
     var password = document.getElementById("form-password").value;
 	var strRegex = "[a-z0-9A-Z]+";
     var re=new RegExp(strRegex);
-    document.cookie = "username" + username;
-    
+
+    //set the cookie for the user
+
+    save_cookies();
     
 	
 	
@@ -64,35 +102,35 @@ document.getElementById("signin_btn").addEventListener("click", function (e) {
 			error_message.innerHTML = "Password format error"
 
 		}
-		else{
-			$.ajax({
-            type: "POST",
-            url: 'http://10.19.42.253:5000/account/login',
-            data: JSON.stringify(text),
-            contentType:"application/jason; charset=UTF-8",
-            async: false,
-            cache: false,
-            processData: false,
-            success: function (data) {
-                if(data.status_code  == 100200){
-                    // 100200不知道咋错了，100211成功 100220密码错误
-                    error_message.innerHTML = "error!";
-                }
-                else if(data.status_code  == 100211){
-                    if (value=='customer'){
-                        window.location.href = "user_management_2/index.html?username="+username;
-                    } else if(value=='employee'){
-                        window.location.href = "../table-basic.html";
-                    }
-
-                }
-                else if(data.status_code  == 100220){
-                    error_message.innerHTML = "Password Wrong!";
-                }
-
-            }
-        })
-		}
+		// else{
+		// 	$.ajax({
+        //     type: "POST",
+        //     url: 'http://10.19.42.253:5000/account/login',
+        //     data: JSON.stringify(text),
+        //     contentType:"application/jason; charset=UTF-8",
+        //     async: false,
+        //     cache: false,
+        //     processData: false,
+        //     success: function (data) {
+        //         if(data.status_code  == 100200){
+        //             // 100200不知道咋错了，100211成功 100220密码错误
+        //             error_message.innerHTML = "error!";
+        //         }
+        //         else if(data.status_code  == 100211){
+        //             if (value=='customer'){
+        //                 window.location.href = "user_management_2/index.html?username="+username;
+        //             } else if(value=='employee'){
+        //                 window.location.href = "../table-basic.html";
+        //             }
+        //
+        //         }
+        //         else if(data.status_code  == 100220){
+        //             error_message.innerHTML = "Password Wrong!";
+        //         }
+        //
+        //     }
+        // })
+		// }
 		
 		
          
