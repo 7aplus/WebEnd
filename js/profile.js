f9cbed1c62a9e9a6722e3c21f7282b701f494b61
lizhongkang <805374598@qq.com>
2019-04-29 12:54:18 +0800

    Profile page

--- js/profile.js
+++ js/profile.js
@@ -4,12 +4,12 @@ window.onload=function () {
     let url=window.location.search;
     if(url.indexOf("?")!==-1){
         result = url.substr(url.indexOf("=")+1);
-        alert(result);
+
     }
     let text = {"type": 'employee','name':result};
-    alert("start");
     let account = document.getElementById('account');
-    account.innerText = result;
+    account.innerHTML = result;
+
     $.ajax({
 
         type: "POST",
@@ -23,9 +23,9 @@ window.onload=function () {
             if (data.status_code === 100200) {
                 alert("ERROR"); // 100200不知道咋错了，100211成功 100220密码错误
             } else if (data.status_code === 'success') {
-                //$('tbody').append('tr');
-                alert(data.id);
-               // showInfo(data);
+
+
+                showInfo(data)
             } else if (data.status_code === 100220) {
                 alert("Password Wrong");
             }
@@ -35,19 +35,22 @@ window.onload=function () {
 };
 
 function showInfo(data) {
-    let fullName = document.getElementById('fullName');
-    fullName.innerHTML = data.firstName+' '+data.lastName;
-    let cardName = document.getElementById('card-name');
-    cardName.innerText = data.firstName+' '+data.lastName;
-    let titleName = document.getElementById('title-name');
-    titleName.innerText = data.firstName+' '+data.lastName;
-    let email = document.getElementById('example-email');
-    email.innerText = data.email;
-    let phone = document.getElementById('phone');
-    phone.innerText = data.phone;
-    let country = document.getElementById('country');
-    let account = document.getElementById('account');
-    account.innerText = data.email;
+    document.getElementById('cardName').innerHTML=data.firstName+' '+data.lastName;
+    // $('#cardName').val(data.firstName+' '+data.lastName);
+    document.getElementById('title-name').innerHTML=data.firstName+' '+data.lastName;
+    $('#fullName').val(data.firstName+' '+data.lastName);
+
+    $('#example-email').val(data.email);
+    $('#phone').val(data.phone);
+
+    if(data.country==='CHINA'){
+        // document.getElementById('country').value='0';
+        $('#country').val(0);
+    }else if (data.country==='IRELAND') {
+        // document.getElementById('ireland').selected=true;
+        $('#country').val(1);
+    }
+
 
     if(data.country==='0'){
         country[0].selected=true;
